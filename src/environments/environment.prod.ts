7b168f671a3f54919fdd7e5a930a08c803a8beac
Anson Shaji <ansonshaji8@gmail.com>
2026-05-03 19:50:53 +0530

    changes in api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 /** Production API origin (EC2 / deployed Django). No trailing slash. */
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://65.0.181.35',
+  apiBaseUrl: 'https://api.lareeshaluxe.com',
 };
